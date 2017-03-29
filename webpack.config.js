0d759333552d053edaa65d8bda89ff7e3c1c4dea
Janno Põldma <janno.poldma@gmail.com>
2017-03-29 11:20:00 +0300

    Initial electron application.

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+﻿var webpack = require("webpack");
+
+module.exports = {
+    entry: "./src/index.ts",
+    output: {
+        filename: "./dist/index.js"
+    },
+    resolve: {
+        extensions: [".ts"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader"
+            }
+        ]
+    },
+    target: "electron"
+}
